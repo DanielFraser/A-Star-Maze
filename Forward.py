90819defcbea96c8f2b3207e7a3097a0cbb66f4e
IHateLogicErrors <dukeydk@gmail.com>
2018-02-14 09:52:37 -0500

    Fog of war is only removed with agent. Now there is 2 functions, 1 for finding a path with current map, and a 2nd for moving the agent.

--- Forward.py
+++ Forward.py
@@ -8,6 +8,7 @@ import Utility
 ACTUAL = np.load('test.npy')
 # create blank board
 currentMap = np.full((101, 101), 0, dtype=np.int8)
+agent = [0, 0]
 openList = []
 hpq.heappush(openList, Utility.Node([0, 0], hi=Utility.distance([0, 0], [100, 100])))
 closed = []  # always empty at start
@@ -41,6 +42,8 @@ def getPath(start):
         cur = cur.parent
         path.append(cur.loc)
 
+    return path.reverse()
+
 # finds a path using a star
 def findPath(start, goal):
     foundGoal = False
@@ -49,8 +52,8 @@ def findPath(start, goal):
         if current.loc == goal: # found goal
             foundGoal = True
             break
-        updateMap(current.loc) # remove fog of war
-        print(currentMap)
+
+        # print(currentMap)
         closed.append(current) # add node to closed list
         possibleMoves = createPossible(current.loc)  # get list of all possible nodes
         for x in possibleMoves:
@@ -59,13 +62,34 @@ def findPath(start, goal):
             node = Utility.Node(x, current, current.g + Utility.distance(current.loc, goal), Utility.distance(x, goal))
             hpq.heappush(openList, node) # add it to binary heap
     if(foundGoal):
-        getPath()
+        return getPath()
     else:
-        print("No path to goal!")
+        return []
+
+# move agent and remove fog of war
+def moveAgent(path):
+    for x in path:
+        x = list(x)
+        updateMap(x) # remove fog of war
+        if currentMap[x[0], x[1]] == 0:
+            global agent
+            agent = x
+        else:
+            return None # we need to find a new path
+
 
 # main method
 if __name__ == '__main__':
-    findPath([0,0], [100,100])
+    start = [0, 0]
+    goal = [100, 100]
+    agent = start
+    updateMap(agent)  # remove fog of war
+    while agent != goal:
+        path = findPath(start, goal) # get path
+        if path:    # if there is a path, move agent
+            moveAgent(path)
+        else:   # no path, unable to get to goal
+            print("No path found")
 
 # process = psutil.Process(os.getpid())
 # print(process.memory_info().rss)
